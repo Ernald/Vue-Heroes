d3bb020ac2a5df58a597af7cd02a264ca4241b59
Ernald Zykaj <ernaldzykaj@gmail.com>
2023-03-19 00:07:32 +0100

    Upload Commit
    Upload Project

---
+++ src/views/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="content-container">
+    <div class="section content-title-group">
+      <h2 class="title">About Vue Project</h2>
+      <p>Here are some helpful resources for this project.</p>
+      <p>
+        by
+        <a href="http://github.com/Ernald">
+          Ernald Zykaj
+        </a>
+      </p>
+      <br />
+      <h3 class="title sub-title">Source Code</h3>
+      <p>You can find the source code for this project at the link below</p>
+      <p>
+        <a
+          href="http://github.com/Ernald" target="_blank">
+          github.com/Ernald
+        </a>
+      </p>
+      <br />
+    </div>
+  </div>
+</template>
